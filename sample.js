48786503a2514d4b174c2c93d72b4061003687c9
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2024-07-08 23:50:00 +0530

    Add files via upload

--- sample.js
+++ sample.js
@@ -1,1 +1,17 @@
-const display = document.getElementById("button")
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function calculate(){
+
+}
+
+function clear(){
+    display.value ="";
+}
+
+function back(){
+
+}
